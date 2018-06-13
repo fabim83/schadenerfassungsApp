4e6ed20b4de993dad58a6dd245e78dfbde6beb39
fabim83 <fabian.metten@gmx.de>
2018-06-13 16:33:26 +0200

    Refactoring Startseite

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -20,8 +20,4 @@ export class TabsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
-
 }
